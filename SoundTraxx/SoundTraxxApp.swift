8c00e428c04dee6c656dfd85d6f3ffa26b8e8b66
GW Rodriguez <gwsounddsg@gmail.com>
2023-10-25 09:30:32 -0400

    Initial Commit

---
+++ SoundTraxx/SoundTraxxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundTraxxApp.swift
+//  SoundTraxx
+//
+//  Created by GW Rodriguez on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SoundTraxxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
